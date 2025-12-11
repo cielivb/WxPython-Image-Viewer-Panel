18317439ac068ce59a9990fd6bae458aa8962fcb
Ciel Baumann <ciel.baumann@gmail.com>
2025-12-12 07:48:42 +1300

    Create module skeleton

--- image_viewer_4.py
+++ image_viewer_4.py
@@ -15,151 +15,68 @@ import cv2
 import wx.lib.inspection
 
 
+
+### Class ViewerPanel (where all the action is!!) ----------------------
+
 class ViewerPanel(wx.Panel):
     def __init__(self, parent, image_file):
         wx.Panel.__init__(self, parent, -1)
-        self.image_file = image_file
-        self.image_height = cv2.imread(image_file).shape[0]        
-        self.image_width = cv2.imread(image_file).shape[1]
-        self.scaled_height = self.image_height
-        self.scaled_width = self.image_width
-        self.SetSize(self.image_width, self.image_height)
-        
-        self.SetBackgroundStyle(wx.BG_STYLE_PAINT)
-        self.size_evt_ct = 0 # Becomes inf after 2 size events
-        self.zoom_factor = 100
-        self.pan_vec = wx.Point2D(0,0)
-        self.in_prog_start = wx.Point(0,0)
-        self.in_prog_vec = wx.Point2D(0,0)
-        self.pan_in_prog = False
-        
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Bind(wx.EVT_GESTURE_ZOOM, self.OnPinch)
-        self.Bind(wx.EVT_LEFT_DCLICK, self.OnDoubleClick)
-        self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
-        self.Bind(wx.EVT_SIZE, self.OnSize)
+        pass
+    
+    
+    ## Normalisation methods --------------------------------------------
+    
+    def NormaliseEventPosition(self, evt_pos):
+        pass
+    
+    ## Paint methods ----------------------------------------------------
     
     def OnSize(self, event):
         self.Refresh()
+        
         
     def GetBitmapPosition(self):
         """Determine bitmap draw location on panel as function of panel 
         size and image size.
         Want centre of image to be drawn on panel_centre.
         """
-        # Get panel centre position
-        panel_size = self.GetSize()
-        panel_width = panel_size[0]
-        panel_height = panel_size[1]
-        panel_centre_x = panel_width/2
-        panel_centre_y = panel_height/2
-        
-        # Get image centre
-        image_centre_x = self.scaled_width/2
-        image_centre_y = self.scaled_height/2
-        
-        # Get draw start coordinates
-        draw_x = panel_centre_x - image_centre_x
-        draw_y = panel_centre_y - image_centre_y
-        return (draw_x, draw_y)
+        pass
+    
     
     def GetBitmapSize(self):
         """Want to rescale based on panel height and width."""
-        
-        # Get panel dimensions
-        panel_size = self.GetSize()
-        panel_width = panel_size[0]
-        panel_height = panel_size[1]
-        
-        # If image is larger in any dimension than the panel, bitmap size
-        # should be modified to fit panel
-        if self.image_height > panel_height or self.image_width > panel_width:
-            # Scale image down to fit the smallest dimension
-            
-            if panel_height <= panel_width:
-                scaled_height = panel_height
-                scale = (self.image_height - panel_height) / self.image_height
-                scaled_width = (1 - scale) * self.image_width
-                
-            else:
-                scaled_width = panel_width
-                scale = (self.image_width - panel_width) / self.image_width
-                scaled_height = (1 - scale) * self.image_height
-                
-            self.scaled_height = scaled_height
-            self.scaled_width = scaled_width
-            
-        else:
-            self.scaled_height = self.image_height
-            self.scaled_width = self.image_width
-        
-        return (self.scaled_height, self.scaled_width)
+        pass
         
     
     def DoDrawCanvas(self, gc):
-        image = gc.CreateBitmap(wx.Bitmap(self.image_file))
-        height, width = self.GetBitmapSize()
-        x, y = self.GetBitmapPosition()
-        gc.DrawBitmap(image, x, y, width, height)
+        pass
+        
         
     def OnPaint(self, event):
-        # Create Paint DC
-        dc = wx.AutoBufferedPaintDC(self)
-        dc.Clear()
+        pass
         
-        # Create graphics context from Paint DC
-        gc = wx.GraphicsContext.Create(dc)
-        if gc:
-            a = self.zoom_factor / 100
-            
-            # Add self.pan_vec and self.in_prog_vec together
-            panvec_tuple = self.pan_vec.Get()
-            inprogvec_tuple = self.in_prog_vec.Get()
-            total_x = panvec_tuple[0] + inprogvec_tuple[0]
-            total_y = panvec_tuple[1] + inprogvec_tuple[1]
-            total_pan = wx.Point2D(total_x, total_y)
-            
-            gc.Translate(-total_pan[0], -total_pan[1])
-            gc.Scale(a, a)
-            self.DoDrawCanvas(gc)
-        
-        del(gc)
-        
+    
+    
+    ### Pan methods ----------------------------------------------------
     
     def ProcessPan(self, position, do_refresh):
         self.in_prog_vec = self.in_prog_start - position
         if do_refresh:
             self.Refresh()
     
+    
     def FinishPan(self, do_refresh):
-        if self.pan_in_prog:
-            self.SetCursor(wx.Cursor(wx.CURSOR_ARROW))
-        if self.HasCapture():
-            self.ReleaseMouse()
-            
-        self.Unbind(wx.EVT_LEFT_UP)
-        self.Unbind(wx.EVT_MOTION)
-        self.Unbind(wx.EVT_MOUSE_CAPTURE_LOST)
-        
-        # Update pan vector (+= in_prog_vec)
-        panvec_tuple = self.pan_vec.Get()
-        inprogvec_tuple = self.in_prog_vec.Get()
-        sum_x = panvec_tuple[0] + inprogvec_tuple[0]
-        sum_y = panvec_tuple[1] + inprogvec_tuple[1]
-        self.pan_vec = wx.Point2D(sum_x, sum_y)
-        
-        self.in_prog_pan_vec = wx.Point2D(0,0)
-        self.pan_in_prog = False
-        
-        if do_refresh: self.Refresh()
+        pass
         
     
     def OnLeftUp(self, event):
         self.ProcessPan(event.GetPosition(), False)
         self.FinishPan(False)
     
+    
     def OnMotion(self, event):
         self.ProcessPan(event.GetPosition(), True)
+    
     
     def OnCaptureLost(self, event):
         self.FinishPan(True)
@@ -167,93 +84,36 @@ class ViewerPanel(wx.Panel):
     
     def OnLeftDown(self, event):
         # Turn the cursor into a hand cursor
-        cursor = wx.Cursor(wx.CURSOR_HAND)
-        self.SetCursor(cursor)
-        
-        self.in_prog_start = event.GetPosition()
-        self.in_prog_vec = wx.Point2D(0,0)
-        self.pan_in_prog = True
-        
-        self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
-        self.Bind(wx.EVT_MOTION, self.OnMotion)
-        self.Bind(wx.EVT_MOUSE_CAPTURE_LOST, self.OnCaptureLost)
-        
-        self.CaptureMouse()
+        pass
     
     
+    
+    ### Zoom methods ---------------------------------------------------
     
     def OnZoom(self, a, b, position):
         """a is the normalised old zoom; b is the normalised new zoom"""
-        # Set panvec so the point below the cursor in the new scaled/panned
-        # corresponds to the same point that is currently below it
-        
-        # st_pt = event.GetPosition() + self.pan_vec
-        pan_vec_tuple = self.pan_vec.Get()
-        event_pos_tuple = position
-        sum_x = pan_vec_tuple[0] + event_pos_tuple[0]
-        sum_y = event_pos_tuple[1] + pan_vec_tuple[1]
-        st_pt = (sum_x, sum_y)
-        
-        xy_pt = (st_pt[0]/a, st_pt[1]/a)
-        new_st_pt = (b*xy_pt[0], b*xy_pt[1])
-        
-        #self.panvec = new_st_pt - event.GetPosition()
-        diff_x = new_st_pt[0] - event_pos_tuple[0]
-        diff_y = new_st_pt[1] - event_pos_tuple[1]
-        self.pan_vec = wx.Point2D(diff_x, diff_y)
-        
-        self.Refresh()
+        pass
+    
         
     def OnPinch(self, event):
-        if self.pan_in_prog : self.FinishPan(False)
-        oldzoom = self.zoom_factor        
-        self.zoom_factor = event.GetZoomFactor()*100
-        
-        # The following snippet accounts for sudden zoom factor shifts
-        if abs(oldzoom - self.zoom_factor) > 20:
-            if oldzoom > self.zoom_factor:
-                self.zoom_factor = oldzoom - 5
-            else:
-                self.zoom_factor = oldzoom + 5
-        
-        a = oldzoom / 100
-        b = self.zoom_factor / 100
-        self.OnZoom(a, b, event.GetPosition())
+        pass
+    
         
     def OnDoubleClick(self, event):
         """Zoom in by 50%"""
-        oldzoom = self.zoom_factor
-        self.zoom_factor = oldzoom * 1.5
-        a = oldzoom / 100
-        b = self.zoom_factor / 100
-        self.OnZoom(a, b, event.GetPosition())
+        pass
         
 
 
+
+
+### Base class supporting ViewerPanel ----------------------------------
 
 class Base(wx.Frame):
     def __init__(self, *args, **kw):
         super().__init__(*args, **kw)
         
-        sizer = wx.BoxSizer(wx.VERTICAL)
         
-        #self.panel = ViewerPanel(self, 'images/small.png')
-        self.panel = ViewerPanel(self, 'images/medium.jpg')
-        #self.panel = ViewerPanel(self, 'images/big.gif') #SHOWS FIRST SCENE ONLY
-        #self.panel = ViewerPanel(self, 'images/nebula.webp') #FAILS
-        
-        self.SetMinSize((300,300))
-        self.SetMaxSize(wx.DisplaySize())
-        
-        self.zoom_out_button = wx.Button(self, label='-', size=(30,30))
-        self.zoom_in_button = wx.Button(self, label='+', size=(30,30))
-        self.zoom_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.zoom_sizer.Add(self.zoom_out_button, 1, wx.ALL, 5)
-        self.zoom_sizer.Add(self.zoom_in_button, 1, wx.ALL, 5)
-        
-        sizer.Add(self.panel, 20, wx.EXPAND)
-        sizer.Add(self.zoom_sizer, 1, wx.ALIGN_CENTRE, 5)
-        self.SetSizer(sizer)
 
 
 def main():
